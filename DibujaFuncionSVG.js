500e0d9f3107df762a3edc90c55bbb5dfc2514b3
Raúl Alegre <raul.alemar@gmail.com>
2014-07-04 09:26:24 +0200

    Add path node

--- DibujaFuncionSVG.js
+++ DibujaFuncionSVG.js
@@ -30,41 +30,79 @@ function dibujaSeno()
 
 function dibujaCatenaria()
 {
-	var svg = document.getElementById('Grafica').children[0];
+	var svg = document.getElementById('Grafica');
 	var origin = { //origin of axes
 	    x: 400,
 	    y: 250
 	};     
-	var rarity = 1;      // point spacing
 
-	var lineas = document.getElementsByTagName('line');
+	/*var lineas = svg.getElementsByTagName('line');
+
+	console.log('Tipo de lineas: ' + typeof(lineas));
+
+	console.log('Antes de hacer remove: ');
+	console.log('Longitud de lineas: ' + lineas.length);
 
 	for (var i = 0; i<lineas.length; i++)
 	{
-		svg.removeChild(lineas[i]);
+		console.log('lineas[i]: ' + lineas[i]);
+		svg.removeChild(lineas[0]);
 	}
 
-	
+
+	console.log('Despues de hacer remove: ');
+	console.log('Longitud de lineas: ' + lineas.length);
+
+	for (var i = 0; i<lineas.length; i++)
+	{
+		console.log('lineas[i]: ' + lineas[i]);
+	}
+
+
+
+	// A partir de aqui tenemos el dibujo usando 'line'
+	// Borramos todo lo que hay en svg
+	while (svg.firstChild)
+	{
+		svg.removeChild(svg.firstChild);
+	}
 
 	for (var i = -300; i < 300; i++)
 	{
 		var line = document.createElementNS("http://www.w3.org/2000/svg", "line");
 
-	    	line.setAttribute('x1', (i - 1) * rarity + origin.x);
+	    	line.setAttribute('x1', (i - 1)  + origin.x);
 	    	line.setAttribute('y1', -Catenaria(i-1) + origin.y); // El signo menos es porque el eje va para abajo
 
-	    	line.setAttribute('x2', i * rarity + origin.x);
+	    	line.setAttribute('x2', i  + origin.x);
 	    	line.setAttribute('y2', -Catenaria(i)   + origin.y); // Idem
 	
 	    	line.setAttribute('style', "stroke:black;stroke-width:3");
 
-	    	svg.appendChild(line);
+	    	//svg.appendChild(line);
+	}*/
+
+	var pathCatenaria = svg.getElementById('pathCatenaria');
+	if(pathCatenaria)
+		svg.removeChild(pathCatenaria);
+
+	// Aqui vamos a intentarlo haciendo path
+	var myPath = document.createElementNS("http://www.w3.org/2000/svg", "path");
+	var ruta = "M" + ((300 - 1)  + origin.x) + " " + (-Catenaria(300-1) + origin.y);
+	for (var i=-300; i<300; i++)
+	{
+		ruta += " L" + ((i)  + origin.x) + " " + (-Catenaria(i) + origin.y)
 	}
+	myPath.setAttribute('style', "stroke:red;stroke-width:3; fill:none");
+	myPath.setAttribute('id', "pathCatenaria");
+	myPath.setAttribute('d', ruta);
+	svg.appendChild(myPath);
+
 }
 
 function dibujaEjes()
 {
-	var svg = document.getElementById('Grafica').children[0];
+	var svg = document.getElementById('Grafica');
 
 	var ejeX = document.createElementNS("http://www.w3.org/2000/svg", "line");
 	var ejeY = document.createElementNS("http://www.w3.org/2000/svg", "line");
