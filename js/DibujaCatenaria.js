a5565ff27a0215e325f3d5e00b55b8debd7d03c1
Raúl Alegre <raul.alemar@gmail.com>
2014-07-08 11:46:54 +0200

    Add plot object

---
+++ js/DibujaCatenaria.js
@@ -0,0 +1,6 @@
+var plotCatenaria = new Plot(Catenaria);
+
+plotCatenaria.creaSVG();
+plotCatenaria.creaText();
+plotCatenaria.creaEjes();
+plotCatenaria.creaPath();
--- js/DibujaFuncionSVG.js
+++ js/DibujaFuncionSVG.js
@@ -110,3 +110,129 @@ function Catenaria(x)
 	var a = Parametro;
 	return a*(Math.exp(x/a) + Math.exp(-x/a)) / 2;
 };
+
+
+function Plot(functionGraph) {
+    
+    this.functionGraph = functionGraph;
+    
+    this.xMin = XMIN;
+    this.xMax = XMAX;
+    this.yMin = YMIN;
+    this.yMax = YMAX;
+
+    this.xRange = function() {
+	return this.xMax - this.xMin;
+    }
+    this.yRange = function() {
+	return this.yMax - this.yMin;
+    }
+
+    this.creaSVG = function() {
+	var divCatenaria = document.getElementById('divCatenaria');
+	var divWidth = divCatenaria.attributes.width.nodeValue;
+	var divHeight = divCatenaria.attributes.height.nodeValue;
+	
+	var svg = document.getElementById('Grafica');
+	svg.setAttribute('width', '100%');
+	svg.setAttribute('height', '90%');
+	svg.setAttribute('viewBox', '' + (this.xMin-0.1*this.xRange()) + 
+			 ' ' + (-this.yMax-0.1*this.yRange()) + ' ' + (1.1*this.xRange()) + ' ' + (1.1)*this.yRange());
+	return svg;
+    }
+
+    var svg = this.creaSVG();
+    
+    this.pathXY = function(i) {
+	var valorX = i;
+	var valorY = this.functionGraph(i);
+	if (valorY < this.yMax && valorY > this.yMin){ 
+	    return "" + valorX + " " + -valorY;
+	}
+    };
+
+    this.creaPath = function() {
+	// Aqui vamos a intentarlo haciendo path
+	var pathCatenaria = svg.getElementById('pathCatenaria');
+	if(pathCatenaria) { svg.removeChild(pathCatenaria) }
+
+	
+	var myPath = document.createElementNS("http://www.w3.org/2000/svg", "path");
+	myPath.setAttribute('style', "stroke:red;stroke-width:1%; fill:none");
+	myPath.setAttribute('id', "pathCatenaria");
+	
+	var ruta = "M";
+	for (var i=0; i<600; i++) {	
+	    var x = XMIN + i*xRange/NUMBER_POINTS;
+	    var xy = this.pathXY(x);
+	    if (xy) {
+		if (ruta!="M") {ruta += " L";}
+		ruta += xy;
+	    }
+	}
+	myPath.setAttribute('d', ruta);
+	svg.appendChild(myPath);
+    }
+
+
+    this.creaText = function() {
+	// Aqui vamos a añadir el texto con el valor del parametro
+	var textoCatenaria = svg.getElementById('textoCatenaria');
+	if(textoCatenaria) { svg.removeChild(textoCatenaria); }
+
+	var myText = document.createElementNS("http://www.w3.org/2000/svg", "text");
+	myText.setAttribute('id', "textoCatenaria");
+	myText.setAttribute('x', XMIN  + 0.01*xRange);
+	myText.setAttribute('y', 0     + 0.10*yRange);
+	myText.setAttribute('font-size', '5%');
+
+	var texto = document.createTextNode('Parámetro: ' + Parametro.toFixed(2));
+	myText.appendChild(texto);
+
+	svg.appendChild(myText);
+    }
+    
+
+
+    this.creaEjes = function() {
+	var ejeX = document.createElementNS("http://www.w3.org/2000/svg", "line");
+	var ejeY = document.createElementNS("http://www.w3.org/2000/svg", "line");
+
+	// Eje X
+	ejeX.setAttribute('x1', XMIN);
+	ejeX.setAttribute('y1', 0);
+	ejeX.setAttribute('x2', XMAX);
+	ejeX.setAttribute('y2', 0);
+
+	// Eje Y
+	ejeY.setAttribute('x1', 0);
+	ejeY.setAttribute('y1', -YMIN);
+	ejeY.setAttribute('x2', 0);
+	ejeY.setAttribute('y2', -YMAX);
+	
+
+	// Colores
+	ejeX.setAttribute('style', "stroke:#006600;stroke-width:0.5%");
+	ejeY.setAttribute('style', "stroke:#006600;stroke-width:0.5%");
+
+	// Las añadimos
+	svg.appendChild(ejeX);
+	svg.appendChild(ejeY);
+
+	//flechas
+	var flechaDelta = 0.0005;
+
+	var flechaX = document.createElementNS("http://www.w3.org/2000/svg", "polygon");
+	var pointsX = ""+(XMAX-flechaDelta)+","+(-flechaDelta)+" "+(XMAX-flechaDelta)+","+flechaDelta+" "+XMAX+",0"
+	console.log(pointsX);
+	flechaX.setAttribute('points', pointsX);
+	flechaX.setAttribute('style',"stroke:#006600;stroke-width:2%;");
+	svg.appendChild(flechaX);
+
+	var flechaY = document.createElementNS("http://www.w3.org/2000/svg", "polygon");
+	var pointsY = ""+(-flechaDelta)+","+(-YMAX+flechaDelta)+" "+flechaDelta+","+(-YMAX+flechaDelta)+" "+" "+"0,"+(-YMAX)
+	flechaY.setAttribute('points', pointsY);
+	flechaY.setAttribute('style',"stroke:#006600;stroke-width:2%;");
+	svg.appendChild(flechaY);
+    }
+}
