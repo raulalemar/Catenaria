4d5edcd699f8030f752e55dc9b2fa24e32c1f6f6
Raúl Alegre <raul.alemar@gmail.com>
2014-07-25 12:47:15 +0200

    adds border to svg

--- js/calculo-mecanico.js
+++ js/calculo-mecanico.js
@@ -8,20 +8,28 @@ cable.section = 54.60;                             // mm^2
 cable.linearDensity = 0.189;                       // kg/m
 cable.diameter = 9.5;                              // mm
 
+var span = 100;
 var conditions1 = new Conditions();
-conditions1.span = 100;                            // m
+conditions1.span = span;                            // m
 
 var conditions2 = new Conditions();
-conditions2.span = 100;
+conditions2.span = span;
 
 
 var tramo;
-var range = new Range(-conditions1.span/2,conditions1.span/2, 20, 40); // entre 0 y 50 deberia ser, estando el poste a 30
+var range = new Range(-conditions1.span/2,conditions1.span/2, 0, 35); // entre 0 y 50 deberia ser, estando el poste a 30
+var sceneRange = new Range(-conditions1.span/2 - 0.1*span,conditions1.span/2 + 0.1*span, 0, 40);
 var constantes;
 var catenariaGraph;
+var posteIzquierdoGraph;
+var posteDerechoGraph;
+var construido = false;
 
 
 var construirDibujo = function (form) {
+
+  construido = true;
+
   var temperature = form.temperature.value;
   var windPressure = form.windPressure.value;
   var tension = form.tension.value;
@@ -36,13 +44,16 @@ var construirDibujo = function (form) {
   tramo = new Tramo(cable, conditions1, conditions2);
   tramo.finalConditions.tension = tramo.solveChangeEquation();
 
-  //console.log();
-
   constantes = resuelvaParabola(tramo.a(), -conditions1.span/2,30, conditions1.span/2, 30);
   catenariaGraph = new CatenaryGraph(tramo.a(), constantes[0], constantes[1]);
+  posteIzquierdoGraph = new Pole(-conditions1.span, 0, 30);
+  posteDerechoGraph   = new Pole(+conditions1.span, 0, 30);
 
   escena.add(catenariaGraph);
-  escena.range = range;
+  escena.add(posteIzquierdoGraph);
+  escena.add(posteDerechoGraph);
+  
+  escena.range = sceneRange;
   catenariaGraph.range = range;
 
   escena.plotSVG();
@@ -75,14 +86,10 @@ function tableCreate(){
   for(var i=0;i<3;i++){
     var tr=document.createElement('tr');
     for(var j=0;j<2;j++){
-      if(i==2 && j==1){
-	break
-      } else {
-	var td=document.createElement('td');
-	td.appendChild(document.createTextNode('\u0020'))
-	i==1&&j==1?td.setAttribute('rowSpan','2'):null;
-	tr.appendChild(td)
-      }
+      var td=document.createElement('td');
+      td.appendChild(document.createTextNode('Hola0:' + i + ',' + j))
+      //i==1&&j==1?td.setAttribute('rowSpan','2'):null;
+      tr.appendChild(td)
     }
     tbdy.appendChild(tr);
   }
